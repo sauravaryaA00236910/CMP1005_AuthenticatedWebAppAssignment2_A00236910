6e9230df117892970a0f7035ef8b110dd2d5aba1
sauravarya <sauravarya22896@gmail.com>
2021-11-19 03:47:43 -0500

    Initial Commit

---
+++ CMP1005_AuthenticatedWebAppAssignment2_A00236910/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CMP1005_AuthenticatedWebAppAssignment2_A00236910.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
